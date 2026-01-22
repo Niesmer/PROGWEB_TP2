c2b874d3b9af50fc25d4c8d533caab5d5b270749
FELIHO Cirine <cirinefeliho@MacBook-Air-de-Cirine.local>
2026-01-22 11:56:56 +0100

    recover

--- Ressources_communes.php
+++ Ressources_communes.php
@@ -2,8 +2,8 @@
 function connectDB($username, $password, $dbname = ""): PDO
 {
     try {
-        $DB_CONNECTION = new PDO("mysql:host=localhost;dbname=$dbname", $username, $password);
-        return $DB_CONNECTION;
+        $db_connection = new PDO("mysql:host=localhost:8889;dbname=$dbname", $username, $password);
+        return $db_connection;
     } catch (PDOException $e) {
         die("Erreur de connexion à la base de données : " . $e->getMessage());
     }
@@ -14,12 +14,12 @@ function InitDB()
     if (!file_exists('base_poly.sql')) {
         die("Le fichier base_poly.sql est introuvable.");
     }
-    $db_connection = connectDB("root", "");
+    $db_connection = connectDB("root", "root");
     $sql = file_get_contents('base_poly.sql');
     try {
-        $db_connection->query("CREATE USER IF NOT EXISTS 'user1'@'localhost' IDENTIFIED BY 'hcetylop'");
-        $db_connection->query("GRANT ALL PRIVILEGES ON *.* TO 'user1'@'localhost' WITH GRANT OPTION");
-        $db_connection->query("CREATE DATABASE IF NOT EXISTS poly_php;");
+        $db_connection->exec("CREATE USER IF NOT EXISTS 'user1'@'localhost' IDENTIFIED BY 'hcetylop'");
+        $db_connection->exec("GRANT ALL PRIVILEGES ON *.* TO 'user1'@'localhost' WITH GRANT OPTION");
+        $db_connection->exec("CREATE DATABASE IF NOT EXISTS poly_php;");
         $db_connection = connectDB("user1", "hcetylop", "poly_php");
         $statements = explode(';', $sql);
         foreach ($statements as $statement) {
