e83524c907a79a2ba5f53522b843f4981ed2f03a
Niesmer <antoine.ferron.pro@pm.me>
2026-01-27 11:35:17 +0100

    add tcpdf

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInit2185d2f99bcd56787481d9357a5972d3::getLoader();
+return ComposerAutoloaderInit6a87123ac6f61b911629411de36bc977::getLoader();
